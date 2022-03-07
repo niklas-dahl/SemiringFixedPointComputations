39c25f56b1e028e5f8c0d2253521d50189bb4a58
Niklas Dahl <niklasdahl97@gmail.com>
2022-03-07 14:40:33 +0100

    init

---
+++ lib.ts
@@ -0,0 +1,155 @@
+export class Vector {
+  constructor(private readonly entries: Polynom[]) {}
+
+  getEntry(n: number) {
+    return this.entries[n];
+  }
+
+  getEntries() {
+    return this.entries;
+  }
+
+  simplify() {
+    return new Vector(this.entries.map((entry) => entry.simplify()));
+  }
+
+  toString() {
+    return "(" + this.entries.map((entry) => entry.toString()).join(", ") + ")";
+  }
+}
+
+export class Polynom {
+  constructor(private readonly monomes: Monome[]) {
+    if (monomes.length === 0) {
+      throw new Error("empty polynome");
+    }
+  }
+
+  add(polynom: Polynom) {
+    return new Polynom([...this.monomes, ...polynom.monomes]);
+  }
+
+  multiply(polynom: Polynom) {
+    // multiply monomes pairwise
+    let resultMonomes: Monome[] = [];
+    this.monomes.forEach((monome) => {
+      polynom.monomes.forEach((monome2) => {
+        resultMonomes.push(monome.multiply(monome2));
+      });
+    });
+    return new Polynom(resultMonomes);
+  }
+
+  simplify() {
+    return new Polynom(this.monomes.filter((monome) => !monome.isZero()));
+  }
+
+  toString() {
+    return this.monomes.map((m) => m.toString()).join(" + ");
+  }
+}
+
+export class Monome {
+  constructor(private readonly parts: VariableExpression[]) {
+    if (parts.length === 0) {
+      throw new Error("empty monome");
+    }
+  }
+
+  multiply(monome: Monome) {
+    return new Monome([...this.parts, ...monome.parts]);
+  }
+
+  simplfy() {
+    return new Monome(this.parts.filter((part) => !part.is0()));
+  }
+
+  isZero() {
+    return this.parts.some((part) => part.is0());
+  }
+
+  toString() {
+    return this.parts.map((m) => m.toString()).join(" * ");
+  }
+}
+
+export class VariableExpression {
+  constructor(
+    private readonly variable: Variable,
+    private readonly exp: number
+  ) {}
+
+  is0() {
+    return this.variable.isBottom();
+  }
+
+  is1() {
+    return this.variable.isTop();
+  }
+
+  toString() {
+    let varStr = this.variable.toString();
+    if (this.exp === 1) {
+      return varStr;
+    }
+    return `${varStr}^${this.exp}`;
+  }
+}
+
+export class Variable {
+  constructor(private readonly name: string) {}
+
+  toPolynom() {
+    return new Polynom([new Monome([new VariableExpression(this, 1)])]);
+  }
+
+  isBottom() {
+    return false;
+  }
+
+  isTop() {
+    return false;
+  }
+
+  toString() {
+    return this.name;
+  }
+}
+
+class BottomVariable extends Variable {
+  isBottom(): boolean {
+    return true;
+  }
+}
+export const Variable_0 = new BottomVariable("0");
+export const Variable_1 = new Variable("1");
+
+// Graph
+
+export class Graph {
+  constructor(private nodes: Node[], private edges: Variable[][]) {}
+
+  getNodeIndex(node: Node): number {
+    return this.nodes.indexOf(node);
+  }
+
+  getNode(nodeIndex: number) {
+    return this.nodes[nodeIndex];
+  }
+
+  getEdges(node: Node): Variable[] {
+    return this.edges[this.getNodeIndex(node)];
+  }
+
+  toString() {
+    return this.nodes.map((node) => node.toString()).join("\n");
+  }
+}
+
+export class Node {
+  constructor(private name: string) {}
+
+  toString() {
+    return this.name;
+  }
+}
---
+++ test.ts
@@ -0,0 +1,133 @@
+import {
+  Graph,
+  Monome,
+  Node,
+  Polynom,
+  Variable,
+  VariableExpression,
+  Variable_0,
+  Variable_1,
+  Vector,
+} from "./lib";
+
+let a = new Variable("a");
+let b = new Variable("b");
+let c = new Variable("c");
+
+// a^2 * b^2 + a^3 * b^1
+let polynom = new Polynom([
+  new Monome([new VariableExpression(a, 2), new VariableExpression(b, 2)]),
+  new Monome([new VariableExpression(a, 3), new VariableExpression(b, 1)]),
+]);
+
+function create0Vector(length: number) {
+  let entries = [];
+  for (let i = 0; i < length; i++) {
+    entries.push(
+      new Polynom([new Monome([new VariableExpression(Variable_0, 1)])])
+    );
+  }
+  return new Vector(entries);
+}
+
+function create1Vector(length: number) {
+  let entries = [];
+  for (let i = 0; i < length; i++) {
+    entries.push(
+      new Polynom([new Monome([new VariableExpression(Variable_1, 1)])])
+    );
+  }
+  return new Vector(entries);
+}
+
+console.log("polynom", polynom.toString());
+
+let nodeV = new Node("v");
+let nodeW = new Node("w");
+
+let graph = new Graph(
+  [nodeV, nodeW],
+  [
+    [a, b],
+    [Variable_0, c],
+  ]
+);
+
+let parityFunction = (node: Node) => {
+  let parityMap: [node: Node, parity: number][] = [
+    [nodeV, 1],
+    [nodeW, 0],
+  ];
+  return parityMap.find((pair) => pair[0] === node)[1];
+};
+let nodeInTargetSet = (node: Node) => {
+  let targetSet = [nodeW];
+  return targetSet.includes(node);
+};
+
+let buchiFixpointIterationFunction = ({
+  graph,
+  node,
+  nodeInTargetSet,
+  Y,
+  Z,
+}: {
+  graph: Graph;
+  node: Node;
+  nodeInTargetSet: (node: Node) => boolean;
+  Y: Vector;
+  Z: Vector;
+}) => {
+  let isPlayer0 = true;
+  let isTargetSet = nodeInTargetSet(node);
+
+  let successors = graph.getEdges(node);
+  let result = new Polynom([]);
+  if (isPlayer0 && isTargetSet) {
+    // sum with Y
+    successors.forEach((edge, nodeIndex) => {
+      // edge * Y[nodeIndex]
+      let edgeValue = edge.toPolynom();
+      let yValue = Y.getEntry(nodeIndex);
+      result = result.add(edgeValue.multiply(yValue));
+    });
+  }
+
+  if (isPlayer0 && !isTargetSet) {
+    // sum with Z
+    successors.forEach((edge, nodeIndex) => {
+      // edge * Z[nodeIndex]
+      let edgeValue = edge.toPolynom();
+      let zValue = Z.getEntry(nodeIndex);
+      result = result.add(edgeValue.multiply(zValue));
+    });
+  }
+
+  if (!isPlayer0) {
+    throw new Error("not implemented");
+  }
+
+  return result;
+};
+
+let Y = create1Vector(2);
+
+function fixpointIteration() {
+  let Z = create0Vector(2);
+
+  console.log("Z", Z.toString());
+
+  let nextZ = Z.getEntries().map((zEntry, nodeIndex) => {
+    return buchiFixpointIterationFunction({
+      graph,
+      node: graph.getNode(nodeIndex),
+      nodeInTargetSet,
+      Y,
+      Z,
+    });
+  });
+
+  let Z_next = new Vector(nextZ);
+  console.log("Z_next", Z_next.simplify().toString());
+}
+fixpointIteration();
